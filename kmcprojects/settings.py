237d51516ffa5fc4df9ac5768fd723063e9f787c
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-03-16 17:43:02 +0000

    (SETUP): add secret key

--- kmcprojects/settings.py
+++ kmcprojects/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b=td%m6jdh+iz*lb1x1mm&&5b5#&2)#_gm$*qj0ddnn_h_x41h'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
