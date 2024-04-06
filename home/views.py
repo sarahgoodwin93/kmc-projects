0e212c39c506350b3793487da82f8be61fcd31c5
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-06 10:46:46 +0000

    (SETUP): added home template and connect home app

--- home/views.py
+++ home/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
 
 # Create your views here.
-def kmc_home(request):
-    return HttpResponse("Hello, World!")
+
+def index(request):
+    """ A view to return the index page """
+
+    return render(request, 'home/index.html')
