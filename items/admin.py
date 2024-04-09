556d276e894002f7f68b253f3ece69cf73222ce1
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-09 05:07:59 +0000

    (FEAT/items): customise admin for items

--- items/admin.py
+++ items/admin.py
@@ -2,5 +2,19 @@ from django.contrib import admin
 from . models import Item, Type
 
 # Register your models here.
-admin.site.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'type',
+        'price',
+        'preferred_supplier',
+        'item_number',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+admin.site.register(Item, ItemAdmin)
 admin.site.register(Type)
