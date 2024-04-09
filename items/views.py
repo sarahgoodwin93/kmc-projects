76bdb9d961dcdb7ddab942230f72cc804e63b717
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-09 06:54:37 +0000

    (FEAT/items): add django templating to items template

--- items/views.py
+++ items/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Item
 
 # Create your views here.
 
 def items(request):
     """ A view to return the items page """
 
-    return render(request, 'items/items.html')
+    items = Item.objects.all()
+
+    context = {
+        'items': items,
+    }
+
+    return render(request, 'items/items.html', context)
