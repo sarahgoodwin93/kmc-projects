36317d2026468938dd4b78b84a6cefa691a75733
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-09 08:07:25 +0000

    (FEAT/items): add search functionality to views and tidy up styling

--- items/views.py
+++ items/views.py
@@ -1,12 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from .models import Item
 
 # Create your views here.
 
 def items(request):
-    """ A view to return the items page """
+    """ A view to return the items page and be able to search for items """
 
     items = Item.objects.all()
+    query = None
+
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "No items found")
+                return redirect(reverse('items'))
+            
+            queries = Q(name__icontains=query) | Q(item_number__icontains=query)
+            items = items.filter(queries)
 
     context = {
         'items': items,
