134cc92c1addf579b557053d3590fd57cb75e563
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-13 12:24:46 +0000

    (FEAT/items): add database pdf urls for items

--- items/models.py
+++ items/models.py
@@ -19,6 +19,7 @@ class Item(models.Model):
     item_number = models.CharField(max_length=254)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
+    datasheet = models.URLField(max_length=1024, null=True, blank=True)
 
     def __str__(self):
         return self.name
