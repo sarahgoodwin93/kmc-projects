ba721a3202026cfa4754e7215eaadda2899493bc
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-16 12:18:35 +0000

    (FEAT/items.json): add datasheet pdf urls links

--- kmcprojects/settings.py
+++ kmcprojects/settings.py
@@ -167,9 +167,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
@@ -180,7 +178,7 @@ if 'USE_AWS' in os.environ:
         'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl': 'max-age=94608000',
     }
-    
+
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'kmcprojects'
     AWS_S3_REGION_NAME = 'eu-north-1'
