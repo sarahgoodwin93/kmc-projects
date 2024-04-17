56311f217d595a4f27d1b6da0e214848fbcc49f4
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-17 13:49:06 +0000

    (FEAT/items): add type search function for item view

--- items/views.py
+++ items/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Item
+from .models import Item, Type
 
 # Create your views here.
 
@@ -10,12 +10,18 @@ def items(request):
 
     items = Item.objects.all()
     query = None
+    type = None
 
     if request.GET:
+        if 'type' in request.GET:
+            type = request.GET['type'].split(',')
+            items = items.filter(type__name__in=type)
+            type = Type.objects.filter(name__in=type)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
-                messages.error(request, "No items found")
+                messages.error(request, "No items found, please enter the name or item number")
                 return redirect(reverse('items'))
             
             queries = Q(name__icontains=query) | Q(item_number__icontains=query)
@@ -23,6 +29,8 @@ def items(request):
 
     context = {
         'items': items,
+        'search_term': query,
+        'current_type': type,
     }
 
     return render(request, 'items/items.html', context)
