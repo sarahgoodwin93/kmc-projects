da0c05dd9f119fb8207983e416e3f7b29e0f696a
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-17 12:41:21 +0000

    (FEAT/home/contact): add contact model

--- home/models.py
+++ home/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Contact(models.Model):
+    """
+    """
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
+    email = models.EmailField(unique=True)
+    message = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
