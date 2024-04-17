b435397bbbd28129082b0296258d60f9172dd514
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-17 13:11:21 +0000

    (FEAT/home/contact): add contact admin model

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Contact
 
 # Register your models here.
+class ContactAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'user',
+        'email',
+        'message',
+        'created_on',
+    )
+
+    ordering = ('created_on',)
+
+admin.site.register(Contact, ContactAdmin)
