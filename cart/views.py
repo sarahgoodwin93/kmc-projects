cbd6567cc7bd2f8e7fe27eb5cbfd5327404f80fa
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-17 19:15:41 +0000

    (FEAT/cart): add remove item view, url and added url to template button

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
 from django.contrib import messages
 
 from items.models import Item
@@ -14,7 +16,7 @@ def cart_view(request):
 def add_to_cart(request, item_id):
     """ Add a quantity of the specified item to the shopping cart """
 
-    item = Item.objects.get(pk=item_id)
+    item = get_object_or_404(Item, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
@@ -27,3 +29,20 @@ def add_to_cart(request, item_id):
 
     request.session['cart'] = cart
     return redirect(redirect_url)
+
+
+def remove_from_cart(request, item_id):
+    """ Remove item from cart """
+
+    item = get_object_or_404(Item, pk=item_id)
+    cart = request.session.get('cart', {})
+
+    if item_id in cart:
+        del cart[item_id]
+        messages.success(request, f'Removed {item.name} from your cart')
+        request.session['cart'] = cart
+    else:
+        messages.error(request, f'{item.name} is not in your cart')
+
+    success_url = reverse("cart")
+    return HttpResponseRedirect(success_url)
