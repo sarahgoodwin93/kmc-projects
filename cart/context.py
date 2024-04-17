2a6adb1e48e4b1a91f9850b3ada360e6bc173b8e
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-17 14:41:33 +0000

    (FEAT/cart): update context processor and cart nav functionality

--- cart/context.py
+++ cart/context.py
@@ -1,16 +1,29 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from items.models import Item
 
 def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    item_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        item = get_object_or_404(Item, pk=item_id)
+        total += quantity * item.price
+        item_count += quantity
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'item': item,
+        })
     
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'item_count': item_count,
     }
 
     return context
