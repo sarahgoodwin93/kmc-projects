08991d681ce13ce512eb138bc50fe31d242bfa8e
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-18 07:21:57 +0000

    (FEAT/orders): add view, url and basic template for orders

--- orders/views.py
+++ orders/views.py
@@ -1,9 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
+from .forms import OrderForm
 
 # Create your views here.
 def OrderView(request):
-    cart = request.session.get('bag', {})
+    cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "No items in the cart")
         return redirect(reverse('items'))
+    
+    order_form = OrderForm()
+    template = 'orders/orders.html'
+    context = {
+        'order_form': order_form,
+    }
+
+    return render(request, template, context)
     
