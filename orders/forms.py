2f5e4bd4dfff67cc03c34181eb880cd666e115d6
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-18 07:09:03 +0000

    (FEAT/orders): add orders form

---
+++ orders/forms.py
@@ -0,0 +1,47 @@
+from django import forms
+from .models import Order
+
+"""
+The base of this code was taken and modified from the walkthrough 
+project Boutiuqe Ado > The Checkout App > Admin, Signals & Forms Part 2.
+Additional code comments have been added to show understanding.
+Parts have been removed that were not relevant to this site and some
+naming has been changed to better suit KMC Projects.  
+"""
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number',
+                  'street_address1', 'street_address2',
+                  'town_or_city', 'postcode', 'country',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Customizes the form's initialization.
+        Sets placeholders and autofocus to start on the full name field.
+        Iterates through the form adding a star to the placeholder if the 
+        field is required. 
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'full_name': 'Full Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'country': 'Country',
+            'postcode': 'Postal Code',
+            'town_or_city': 'Town or City',
+            'street_address1': 'Street Address 1',
+            'street_address2': 'Street Address 2',
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
+
