c1b1a335d97ffd2004014687c3cea585545d24cb
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-27 12:49:29 +0000

    (FEAT/orders): install stripe and set up views.py

--- kmcprojects/settings.py
+++ kmcprojects/settings.py
@@ -208,3 +208,11 @@ if 'USE_AWS' in os.environ:
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Stripe
+FREE_DELIVERY_THRESHOLD = 50
+STANDARD_DELIVERY_PERCENTAGE = 10
+STRIPE_CURRENCY = 'nzd'
+STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
+STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+
--- orders/views.py
+++ orders/views.py
@@ -3,20 +3,38 @@ from django.http import HttpResponseRedirect
 from django.contrib import messages
 from .forms import OrderForm
 from items.models import Item
+from cart.context import cart_contents
+from django.conf import settings
+import stripe
 
 # Create your views here.
 def OrderView(request):
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     cart = request.session.get('cart', {})
     if not cart:
         messages.error(request, "No items in the cart")
         return redirect(reverse('cart'))
     
+    current_order = cart_contents(request)
+    total_order = current_order['total']
+    stripe_total = round(total_order * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
     order_form = OrderForm()
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Did you forget to set it in your environment?')
     template = 'orders/orders.html'
     context = {
         'order_form': order_form,
-        'stripe_public_key': 'pk_test_51P7BM7RxdvIivDmZZ36WieRTd4RuHqaYl7BeqKPOywzGJCf6avcE38BK9PcXCuBAlFSEXKoKvb3jGKR7dwCoonzQ00wKGFIZvf',
-        'client_secret': 'sk_test_51P7BM7RxdvIivDmZ7s7PeCu7pfDZODnKIK79Cm43ypalOyJIYa5yUYC6DuYb5cTdACiYyfIshvnvuUiTdHcctYCp006NDqoXjZ',
+        'stripe_public_key': stripe_public_key,
+        'client_secret': intent.client_secret,
     }
 
     return render(request, template, context)
