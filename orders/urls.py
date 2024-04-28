948d203483175722ae5c33407682867c348b5b20
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-28 16:33:02 +0000

    Revert "(FEAT/order) add webhook and webhook handler method and views"
    This reverts commit bc5cf5021b1e712ab38dbe6debaf81b099e94fe8.

--- orders/urls.py
+++ orders/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
 from . import views
 from .views import OrderView
-from .webhooks import webhook
 
 urlpatterns = [
     path('', views.OrderView, name='orders'),
     path('remove/<item_id>/', views.remove_from_order, name='remove_from_order'),
     path('order_success/<order_number>', views.order_success, name='order_success'),
-    path('wh/', webhook, name='webhook'),
 ]
