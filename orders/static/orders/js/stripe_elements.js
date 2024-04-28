59b4482c0d551e42204d327250936ac64bfccf17
Sarah Goodwin <sgoodwin@crowdcomms.co.uk>
2024-04-28 17:30:49 +0000

    (FIX/orders): match syntax for client_secret

--- orders/static/orders/js/stripe_elements.js
+++ orders/static/orders/js/stripe_elements.js
@@ -6,8 +6,8 @@
     https://stripe.com/docs/stripe-js
 */
 
-var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
-var clientSecret = $('#id_client_secret').text().slice(1, -1);
+var stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+var client_secret = $('#id_client_secret').text().slice(1, -1);
 var stripe = Stripe(stripe_public_key);
 var elements = stripe.elements();
 var style = {
@@ -51,7 +51,7 @@ form.addEventListener('submit', function(ev) {
     ev.preventDefault();
     card.update({ 'disabled': true});
     $('#submit-button').attr('disabled', true);
-    stripe.confirmCardPayment(clientSecret, {
+    stripe.confirmCardPayment(client_secret, {
         payment_method: {
             card: card,
         }
